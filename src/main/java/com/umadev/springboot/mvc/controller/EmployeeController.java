67d46de6184872bc1b6c4a0e6b60ac7ca90dcbe2
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-25 13:02:25 -0600

    Added support for deleting operations

--- src/main/java/com/umadev/springboot/mvc/controller/EmployeeController.java
+++ src/main/java/com/umadev/springboot/mvc/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,5 +74,15 @@ public class EmployeeController {
 		// Use redirect to prevent duplicate submissions
 		// Redirect to request mapping /employees/list
 		return  "redirect:/employees/list";
+	}
+
+	@GetMapping("/delete")
+	public String delete (@RequestParam("employeeId") int theId, Model theModel) {
+		
+		// delete the employee
+		employeeService.deleteById(theId);
+		
+		// redirect to employees/list
+		return "redirect:/employees/list"; 
 	}
 }
