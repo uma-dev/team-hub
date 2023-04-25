b337caa8750773ce97cb7b6b83c958adb6e41860
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-25 12:11:38 -0600

    Added support to update via POST and reusing employee form

--- src/main/java/com/umadev/springboot/mvc/controller/EmployeeController.java
+++ src/main/java/com/umadev/springboot/mvc/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.umadev.springboot.mvc.entity.Employee;
 import com.umadev.springboot.mvc.service.EmployeeService;
@@ -48,6 +49,19 @@ public class EmployeeController {
 		// for binding data
 		theModel.addAttribute("employee", theEmployee);
 
+		return "employees/employee-form"; 
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate (@RequestParam("employeeId") int theId, Model theModel) {
+		
+		// get the employee from the service
+		Employee  theEmployee = employeeService.findById(theId);
+		
+		// set employee in the model to pre-populate the form 
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form 
 		return "employees/employee-form"; 
 	}
 
