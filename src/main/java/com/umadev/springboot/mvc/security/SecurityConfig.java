156f7052fb20755c80efdfa175f759af6eb7fc42
Omar Roldán Guerra <omar@Omars-MacBook-Air.local>
2023-04-28 19:52:41 -0600

    Added Security filter chain to include form login

--- src/main/java/com/umadev/springboot/mvc/security/SecurityConfig.java
+++ src/main/java/com/umadev/springboot/mvc/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.umadev.springboot.mvc.security;
 
-
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -11,57 +10,66 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 public class SecurityConfig {
 
-     // Add support  for JDBC, to not hardcode users. This code doesn't change with pwd encryption
-     @Bean 
-     UserDetailsManager userDetailsManager (DataSource dataSource) {
+    // Add support for JDBC, to not hardcode users. This code doesn't change with
+    // pwd encryption
+    @Bean
+    UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-         // Queries for using custom tables/schema
-         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
- 
-         // Define query to retrieve a user by username
-         jdbcUserDetailsManager.setUsersByUsernameQuery(
-             // Question mark is the parameter value from login
-             "select user_id, pw, active from members where user_id=?"
-         );
- 
-         // Define query to retrieve the authorities/roles
-         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-             // Question mark is the parameter value from login
-             "select user_id, role from roles where user_id=?"
-         );
- 
-         return jdbcUserDetailsManager;
-     }
- 
-     @Bean 
-    public SecurityFilterChain filterChain( HttpSecurity http) throws Exception { 
-                  
-        http.authorizeHttpRequests( configurer -> 
-            configurer
-            // anyRequest().authenticated() Adds suport for CSS in Spring security
-                 .requestMatchers(HttpMethod.GET, "/hello").hasRole("EMPLOYEE").anyRequest().authenticated()
-                 //.requestMatchers(HttpMethod.GET, "/employees/list").hasRole("EMPLOYEE")
-                 //.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                 //.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                 // In case you are passing ID in JSON (service branch)
-                 //.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
- 
-                 // If passing ID by URL: 
-                 //.requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
-                 //.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-         );
-        
-         // use HTTP basic authentication
-         http.httpBasic();
- 
-         // Disable CSRF
-         // In general not required for stateless REST API that use POST, PUT, DELETE and/or PATCH
-         http.csrf().disable();
-         return http.build();
-     }
- 
+        // Queries for using custom tables/schema
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // Define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                // Question mark is the parameter value from login
+                "select user_id, pw, active from members where user_id=?");
+
+        // Define query to retrieve the authorities/roles
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                // Question mark is the parameter value from login
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .anyRequest().authenticated()
+
+        // .requestMatchers(HttpMethod.GET, "/employees/list").hasRole("EMPLOYEE")
+        // .requestMatchers(HttpMethod.GET,
+        // "/resources/**").permitAll().anyRequest().permitAll()
+
+        // .requestMatchers(HttpMethod.GET, "/employees/list").hasRole("EMPLOYEE")
+        // .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+        // .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+        // In case you are passing ID in JSON (service branch)
+        // .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+
+        // If passing ID by URL:
+        // .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+        // .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        )
+                .formLogin(form -> form
+                        // Need a controller
+                        .loginPage("/showMyLoginPage")
+                        // Login form should POST data to this URL for processing
+                        .loginProcessingUrl("/authenticateTheUser")
+                        // Allow everyone to see the page
+                        .permitAll());
+
+        // use HTTP basic authentication
+        // http.httpBasic();
+
+        // Disable CSRF
+        // In general not required for stateless REST API that use POST, PUT, DELETE
+        // and/or PATCH
+        // http.csrf().disable();
+        return http.build();
+    }
+
 }
