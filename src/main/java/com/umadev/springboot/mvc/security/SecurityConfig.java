6ca08037244d98d9a1e5e6db55d9fd90be345083
Omar Roldán Guerra <omar@Omars-MacBook-Air.local>
2023-04-29 21:18:33 -0600

    Removed unnecesary import

--- src/main/java/com/umadev/springboot/mvc/security/SecurityConfig.java
+++ src/main/java/com/umadev/springboot/mvc/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
